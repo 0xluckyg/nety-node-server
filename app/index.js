b57acda96cb87662181c32c9b0639f820c72d884
Scott Cho <scottsgcho@gmail.com>
2017-06-29 15:04:24 -0400

    cors

--- app/index.js
+++ app/index.js
@@ -23,6 +23,14 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, FETCH, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Expose-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 const server = http.createServer(app);
 const io = socketIO(server);

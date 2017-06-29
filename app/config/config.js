07b1845c67419246a87fcd7c292a65bfd1d64a86
Scott Cho <scottsgcho@gmail.com>
2017-06-29 15:14:32 -0400

    jwt fix

--- app/config/config.js
+++ app/config/config.js
@@ -1,11 +1,9 @@
 const env = process.env.NODE_ENV || 'development';
 console.log('env ********', env);
 
-if (env === 'development' || env === 'test') {
-    const config = require('./config.json');
-    const envConfig = config[env];
+const config = require('./config.json');
+const envConfig = config[env];
 
-    Object.keys(envConfig).forEach((key) => {
-        process.env[key] = envConfig[key];
-    });
-}
+Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+});
